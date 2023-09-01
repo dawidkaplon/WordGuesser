09a00cc006b905402111b93bc7fd2725992a78c6
Dawid Kapłon <dawidkaplon2@gmail.com>
2023-09-01 15:04:36 +0200

    Create basic unit tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+import random
+
+from .scraping.webscraper import WebScraper
 
 # Create your tests here.
+
+class WordTest(TestCase):
+    
+    def test_word_length(self):
+        """Verify that length of the word is same as the number given as method's parameter"""
+        
+        random_length = random.randint(5, 8)
+        webscraper = WebScraper()
+        webscraper.fetch_data(random_length)
+        self.assertEqual(random_length, len(WebScraper.result_word))
+
+    def test_word_spelling(self):
+        """Verify that there are no whitespaces, commas, special symbols etc. in the word"""
+        
+        self.assertTrue(WebScraper.result_word.isalpha())
+
+    
