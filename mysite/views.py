30d27643254c3143af734557ccf778e0c901e557
Dawid Kapłon <dawidkaplon2@gmail.com>
2023-09-13 12:11:05 +0200

    Make letter validation working: input-boxes now correctly changes the color after guess

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from copy import deepcopy
 
 # Create your views here.
 
@@ -10,7 +9,8 @@ def index(request):
 
 class Game:
     reset_flag = True
-    active_rows = 1  # Current row user is already guessing (from 1-5)
+    active_rows = 1  # Current row user is already guessing (1-5)
+    box_colors = {}
 
     def gameplay(request):
         global letters
@@ -20,36 +20,31 @@ class Game:
             word_length = [str(num + 1) for num in range(len(word["word"]))]
             if Game.reset_flag:
                 Game.active_rows = 1
+                Game.box_colors = {}
                 letters = {}
                 for row in "12345":
                     for index in word_length:
                         # If user didnt guess any letter yet, input values should be an empty strings
                         letters[f"row{row}col{index}"] = ""
 
-            if "word" in request.session:
-                # del request.session["word"]
-                pass
-
             if request.method == "POST":
-                Game.reset_flag = False  # Avoid clearing the letters dict if user already make a guess
+                Game.reset_flag = False  # Avoid clearing the letters dict if user already made a guess
                 if "" in request.POST.values():
                     pass
                 else:
-                    Game.active_rows += 1
                     for key, value in request.POST.items():
-                        letters[key] = value.upper()
+                        if 'token' not in key:
+                            letters[key] = value.upper()
                     for key, value in letters.items():
-                        print(key, value)
-                    
-                    """
-                     todo: make validating the letters system: 
-                     check if user's letter is present in current word;
-                     if it is, check if letter is at valid position. 
-                     If yes, make input field green, if no, make it orange
-                     If letter is not present in the word, leave the input box grey.
-                     Imo it should be done by comparing last values added 
-                     to the dictionary (by column indexes) with actual letters in the word
-                    """
+                        if f'row{Game.active_rows}' in key:
+                            if value in word['word']:
+                                if word['word'][int(key[-1]) - 1] == value:
+                                    Game.box_colors[key] = 'lightgreen'
+                                else:
+                                    Game.box_colors[key] = 'orange'
+                            else:
+                                Game.box_colors[key] = 'grey'
+                    Game.active_rows += 1
 
             return render(
                 request,
@@ -59,6 +54,7 @@ class Game:
                     "word_length": word_length,
                     "letters": letters,
                     "active_rows": Game.active_rows,
+                    "box_colors": Game.box_colors,
                 },
             )
 
