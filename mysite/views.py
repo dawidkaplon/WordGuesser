ee6642f93d96919762bc372f71bb4f1dbc28b594
Dawid Kapłon <dawidkaplon2@gmail.com>
2023-09-21 17:37:36 +0200

    Add win & loss scenarios

--- mysite/views.py
+++ mysite/views.py
@@ -6,11 +6,18 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request, "index.html")
 
-
 class Game:
     reset_flag = True
-    active_rows = 1  # Current row user is already guessing (1-5)
+    active_rows = 1  # Current row user is guessing
     box_colors = {}
+    keyboard_colors = {}
+    
+    win = 0  
+    """
+    Kind of flag to check if user correctly guessed all letters.
+    If at least one letter is wrong, this variable will be changed to 1
+    """
+    
 
     def gameplay(request):
         global letters
@@ -18,17 +25,20 @@ class Game:
         try:
             word = request.session["word"]
             word_length = [str(num + 1) for num in range(len(word["word"]))]
+            
             if Game.reset_flag:
                 Game.active_rows = 1
                 Game.box_colors = {}
+                Game.win = 0
                 letters = {}
                 for row in "12345":
                     for index in word_length:
                         # If user didnt guess any letter yet, input values should be an empty strings
                         letters[f"row{row}col{index}"] = ""
-
+                
             if request.method == "POST":
                 Game.reset_flag = False  # Avoid clearing the letters dict if user already made a guess
+                Game.win = 0
                 if "" in request.POST.values():
                     pass
                 else:
@@ -40,11 +50,36 @@ class Game:
                             if value in word['word']:
                                 if word['word'][int(key[-1]) - 1] == value:
                                     Game.box_colors[key] = 'lightgreen'
+                                    Game.keyboard_colors[value] = 'lightgreen'
                                 else:
                                     Game.box_colors[key] = 'orange'
+                                    Game.keyboard_colors[value] = 'orange'
+                                    Game.win = 1
                             else:
                                 Game.box_colors[key] = '#C0C0C0'
+                                Game.keyboard_colors[value] = 'grey'
+                                Game.win = 1
                     Game.active_rows += 1
+                    
+                    if Game.win == 0:
+                        """Win case"""
+                        return render(
+                            request, 
+                            "win_page.html",
+                            {
+                                "word": word
+                            }
+                        )
+                    
+                    if Game.active_rows == 6:
+                        """Loss case"""
+                        return render(
+                            request, 
+                            "loss_page.html",
+                            {
+                                "word": word
+                            }
+                        )
 
             return render(
                 request,
